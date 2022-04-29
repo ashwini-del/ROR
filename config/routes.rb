508940ef7e1526f6bd93559ca2a7979f99b8b47b
ashwini-del <71241700+ashwini-del@users.noreply.github.com>
2022-04-29 18:46:13 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :posts
-  root  to: "articles#index"
+  #resources :posts
+  #root  to: "posts#index"
+  puts "Congratulation you successfully depoy Rails application
   
 end
