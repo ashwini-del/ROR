488b492a261e2a64e30ff4aa7b6e9b61e3c89a40
ashwini-del <71241700+ashwini-del@users.noreply.github.com>
2022-04-29 18:37:22 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+Congratulations  you have Successfully deploy Ruby application
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  Congratulations  you have Successfully deploy Ruby application
+  
 end
